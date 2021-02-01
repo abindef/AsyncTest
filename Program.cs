8b69cf83952e3d2200b165ad1431807287676818
abindef <abindef@163.com>
2021-02-01 13:53:23 +0800

    Demo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncTest
 {
@@ -6,7 +8,46 @@ namespace AsyncTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            //模拟并发
+            while (true)
+            {
+                Task.Run(Producer);
+                //Task.Run(Producer2);
+                Thread.Sleep(200);
+            }
+           
+        }
+        /// <summary>
+        /// 如果处理程序耗时>请求耗时，也就是说引起并发，就会导致死锁
+        /// </summary>
+        static void Producer()
+        {
+            var result = Process().Result;
+            //或者
+            //Process().Wait();
+        }
+        static void Producer2()
+        {
+            var result = Process().ConfigureAwait(false);
+        }
+        //cpu 内存均正常
+        static async Task Producer3()
+        {
+            await Process();
+        }
+
+        static async Task<bool> Process()
+        {
+            Console.WriteLine("Start - " + DateTime.Now.ToLongTimeString());
+            await Task.Run(() =>
+            {
+                //模拟任务执行耗时
+                Thread.Sleep(1000);
+            });
+
+            Console.WriteLine("Ended - " + DateTime.Now.ToLongTimeString());
+            return true;
         }
     }
 }
