35004afe11295ae2e619d4d3aa6bf64358e6d08e
abindef <abindef@163.com>
2021-02-25 16:27:53 +0800

    1、增加线程监控 2、增加线程池处理方式

--- Program.cs
+++ Program.cs
@@ -13,10 +13,11 @@ namespace AsyncTest
             while (true)
             {
                 Task.Run(Producer);
-                //Task.Run(Producer2);
+                 //Task.Run(Producer2);
+                //Task.Run(ProducerQueue);
                 Thread.Sleep(200);
             }
-           
+
         }
         /// <summary>
         /// 如果处理程序耗时>请求耗时，也就是说引起并发，就会导致死锁
@@ -27,6 +28,9 @@ namespace AsyncTest
             //或者
             //Process().Wait();
         }
+        /// <summary>
+        /// 正常
+        /// </summary>
         static void Producer2()
         {
             var result = Process().ConfigureAwait(false);
@@ -37,17 +41,76 @@ namespace AsyncTest
             await Process();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        static void ProducerQueue()
+        {
+            ProcessQueue().Wait();
+            //或者
+            //Process().Wait();
+        }
+        /// <summary>
+        /// 正常
+        /// </summary>
+        static void ProducerQueue2()
+        {
+            var result = ProcessQueue().ConfigureAwait(false);
+        }
+        //cpu 内存均正常
+        static async Task ProducerQueue3()
+        {
+            await ProcessQueue();
+        }
+
         static async Task<bool> Process()
         {
-            Console.WriteLine("Start - " + DateTime.Now.ToLongTimeString());
+            ConsoleWrite("Start");
             await Task.Run(() =>
             {
                 //模拟任务执行耗时
                 Thread.Sleep(1000);
             });
 
-            Console.WriteLine("Ended - " + DateTime.Now.ToLongTimeString());
+            ConsoleWrite("End");
             return true;
+        }
+
+      
+
+        static async Task ProcessQueue()
+        {
+            Console.WriteLine("Start - " + GetCurrentThreadID());
+
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                Console.WriteLine("Hello, " + (string)state + GetCurrentThreadID());
+            }, await GetName());
+            Console.WriteLine("Ended - " + GetCurrentThreadID());
+        }
+
+        private static async Task<string> GetName()
+        {
+            Thread.Sleep(1000);
+            return "ZhiXin" + GetCurrentThreadID();
+        }
+        private static string GetCurrentThreadID()
+        {
+            return $" {DateTime.Now.ToLongTimeString()} --ThreadId[{Thread.CurrentThread.ManagedThreadId.ToString("0000")}]";
+        }
+        private static void ConsoleWrite(string type)
+        {
+            if (type == "Start")
+            {
+                //Console.BackgroundColor = ConsoleColor.Blue; //设置背景色
+                Console.ForegroundColor = ConsoleColor.Green; //设置前景色，即字体颜色
+            }
+            else
+            {
+               // Console.BackgroundColor = ConsoleColor.Red; //设置背景色
+                Console.ForegroundColor = ConsoleColor.Red; //设置前景色，即字体颜色
+            }
+            Console.WriteLine($"{type} - " + GetCurrentThreadID());
         }
     }
 }
